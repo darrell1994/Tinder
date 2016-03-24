aafa351623e962078e7d188b1d3d0e85d7efa1c2
Darrell Shi <darrell_authentic@yahoo.com>
2016-03-24 17:04:10 -0400

    milestone 5

--- Tinder/DraggableView.swift
+++ Tinder/DraggableView.swift
@@ -29,8 +29,7 @@ class DraggableView: UIView {
         contentView.frame = bounds
         addSubview(contentView)
             
-            // custom initialization logic
-        
+        // custom initialization logic
         let gesture = UIPanGestureRecognizer(target: self, action: #selector(DraggableView.onDraggingImage(_:)))
         imageView.addGestureRecognizer(gesture)
     }
@@ -42,13 +41,27 @@ class DraggableView: UIView {
     
     var originalCenter: CGPoint?
     func onDraggingImage(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translationInView(self)
+
         if gesture.state == .Began {
             originalCenter = imageView.center
         } else if gesture.state == .Changed {
-            let translation = gesture.translationInView(self)
             imageView.center = CGPoint(x: originalCenter!.x+translation.x, y: originalCenter!.y)
+            imageView.transform = CGAffineTransformMakeRotation(CGFloat(Double(translation.x)/180.0*M_PI)/6)
         } else if gesture.state == .Ended {
-            
+            if translation.x > 50 {
+                UIView.animateWithDuration(0.35, animations: {
+                    self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI/2))
+                    self.imageView.center = CGPoint(x: UIScreen.mainScreen().bounds.width+200, y: self.originalCenter!.y)
+                })
+            } else if translation.x < -50 {
+                UIView.animateWithDuration(0.35, animations: {
+                    self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(-M_PI/2))
+                    self.imageView.center = CGPoint(x: -200, y: self.originalCenter!.y)
+                })
+            } else {
+                imageView.transform = CGAffineTransformIdentity
+            }
         }
     }
 }
